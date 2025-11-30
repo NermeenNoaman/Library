238486e0eb8e68f324013bfbaa50d87b3638bcb3
Alshimaa Sobhy <alshimaasalamaa@gmail.com>
2025-11-30 21:45:19 +0200

    Delete Library_management directory

--- Library_management/models/librarianModel.js
+++
@@ -1,8 +0,0 @@
-const db=require('../config/db'); const bcrypt=require('bcrypt');
-module.exports={
- createLibrarian:async({username,password})=>{
-  const hashed=await bcrypt.hash(password,10);
-  await db.query("INSERT INTO librarian (username,password) VALUES (?,?)",[username,hashed]);
- },
- getByUsername:async(u)=>{ const[r]=await db.query("SELECT * FROM librarian WHERE username=?",[u]); return r[0]; }
-};
--- Library_management/routes/memberRoutes.js
+++
@@ -1,2 +0,0 @@
-const e=require('express'); const r=e.Router(); const c=require('../controllers/memberController');
-r.get('/',c.getMembers); r.post('/',c.createMember); module.exports=r;
